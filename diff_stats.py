af9127c9c944159cffa00de65b2a05b9b672306e
Marc-Antoine Ruel <maruel@gmail.com>
2011-02-17 11:12:22 -0500

    Improves python scripts and make them more PEP8 compliant

--- diff_stats.py
+++ diff_stats.py
@@ -3,8 +3,6 @@
 # source code is governed by a BSD-style license that can be found in the
 # LICENSE file.
 
-"""Prints the diff stats for the pipelined branches."""
-
 import re
 import string
 import subprocess
@@ -12,40 +10,44 @@ import sys
 
 
 def check_capture(args, **kwargs):
-  kwargs['stdout'] = subprocess.PIPE
-  kwargs['stderr'] = subprocess.STDOUT
-  proc = subprocess.Popen(args, **kwargs)
-  out = proc.communicate()
-  if proc.returncode:
-    raise Exception('Oh Fuck; Acid kicks in')
-  return out[0]
+    kwargs['stdout'] = subprocess.PIPE
+    kwargs['stderr'] = subprocess.STDOUT
+    proc = subprocess.Popen(args, **kwargs)
+    out = proc.communicate()
+    if proc.returncode:
+        raise Exception('Oh Fuck; Acid kicks in')
+    return out[0]
 
 
 def get_branches(cwd):
-  return [
-      i[2:] for i in check_capture(['git', 'branch'], cwd=cwd).splitlines()]
+    return [
+        i[2:] for i in check_capture(['git', 'branch'], cwd=cwd).splitlines()
+    ]
 
 
 def diff_stats(cwd):
-  branches = get_branches(cwd)
-  for branch in branches:
-    match = re.match(r'^(\d+)(\w*?)_.*$', branch)
-    if match:
-      branch_number = int(match.group(1))
-      if not branch_number:
-        continue
-      postfix = match.group(2)
-      prefix = '%d%s_' % ((branch_number - 1), postfix)
-      prev_branches = [i for i in branches if i.startswith(prefix)]
-      if len(prev_branches) == 1:
-        delta = '%s..%s' % (prev_branches[0], branch)
-        print delta
-        sys.stdout.flush()
-        # Force color so diff_stats.py | less produces useful output when
-        # os.environ['LESS'] == '-R'.
-        subprocess.check_call(
-            ['git', 'diff', '--stat', '--color', delta], cwd=cwd)
+    """Prints the diff stats for the pipelined branches."""
+    branches = get_branches(cwd)
+    for branch in branches:
+        match = re.match(r'^(\d+)(\w*?)_.*$', branch)
+        if match:
+            branch_number = int(match.group(1))
+            if not branch_number:
+                continue
+            postfix = match.group(2)
+            prefix = '%d%s_' % ((branch_number - 1), postfix)
+            prev_branches = [i for i in branches if i.startswith(prefix)]
+            if len(prev_branches) == 1:
+                delta = '%s..%s' % (prev_branches[0], branch)
+                print delta
+                sys.stdout.flush()
+                # Force color so diff_stats.py | less produces useful output
+                # when os.environ['LESS'] == '-R'.
+                subprocess.check_call(
+                    ['git', 'diff', '--stat', '--color', delta], cwd=cwd)
 
 
 if __name__ == '__main__':
-  diff_stats(None)
+    diff_stats(None)
+
+# vim: ts=4:sw=4:tw=80:et:
--- update_config.py
+++ update_config.py
@@ -8,38 +8,53 @@
 import os
 import shutil
 import subprocess
-from maruel import *
+import maruel
 
 
-def update_config(config_dir):
-  home_dir = os.path.expanduser('~')
-  basefilename = os.path.basename(__file__)
-  existing = []
-  for basename in os.listdir(config_dir):
-    src = os.path.join(config_dir, basename)
-    dst = os.path.join(home_dir, basename)
-    if os.path.isdir(src):
-      # TODO(maruel): !!
-      continue
-    if basename in (basefilename, 'README'):
-      continue
-    if not os.path.exists(dst):
-      print('Copying %s' % basename)
-      shutil.copyfile(src, dst)
-    else:
-      existing.append((basename, src, dst))
+def update_config(config_dir, incremental):
+    """Processes files in config_dir and copy them in the $HOME directory.
 
-  for basename, src, dst in existing:
-    if read(src) != read(dst):
-      # Diff. Note that diff will return 1 when there's a diff.
-      print('Diffing %s' % basename)
-      subprocess.call(['diff', src, dst])
+    If incremental is True, assume the content should be _inside_ the target
+    content. Otherwise, assume the content should be at the beginning of the
+    target content.
+    """
+    home_dir = os.environ['HOME']
+    existing = []
+    for basename in maruel.walk(config_dir, [r'.*'], [r'README$']):
+        src = os.path.join(config_dir, basename)
+        dst = os.path.join(home_dir, basename)
+        if os.path.isdir(src) and not os.path.isdir(dst):
+            print('Creating directory %s' % dst)
+            os.makedirs(dst)
+            continue
+        if not os.path.exists(dst):
+            print('Copying %s' % basename)
+            shutil.copyfile(src, dst)
+        else:
+            # Will process afterwards.
+            existing.append((basename, src, dst))
+
+    def check(src, dst):
+        if incremental:
+            return maruel.read(dst).startswith(maruel.read(dst))
+        else:
+            return maruel.read(dst).endswith(maruel.read(dst))
+
+    for basename, src, dst in existing:
+        print('Verifying %s' % basename)
+        if not check(src, dst):
+            # Diff. Note that diff will return 1 when there's a diff.
+            print('Diffing %s' % basename)
+            subprocess.call(['diff', src, dst])
+
 
 
 def main():
-  curpath = os.path.dirname(os.path.abspath(__file__))
-  update_config(os.path.join(curpath, 'configs'))
+    curpath = os.path.dirname(os.path.abspath(__file__))
+    update_config(os.path.join(curpath, 'configs'), False)
 
 
 if __name__ == '__main__':
-  main()
+    main()
+
+# vim: ts=4:sw=4:tw=80:et:
