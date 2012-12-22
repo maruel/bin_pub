6b0ee999d996dba6dfb36bc2089ccdbeb8f3c9fe
Marc-Antoine Ruel <maruel@gmail.com>
2012-12-21 21:53:42 -0500

    Update rsync.py to disable delete-by-default

--- rsync.py
+++ rsync.py
@@ -10,72 +10,82 @@ import subprocess
 import sys
 
 
-def rsync(src, dst,
+def rsync(
+    src, dst,
+    delete,
     resume=False,
     excludes=('*bak', '*~'),
     quiet=False,
     bandwidth=None):
-    """Calls rsync correctly, sync everything in src into dst.
-    http://samba.anu.edu.au/ftp/rsync/rsync.html
-    
-    Args:
-    - resume will resume (append to) the file copy instead of starting over
-    - quiet remove progress, for cron job
-    - Bandwidth is in KBytes per second.
-    """
-    # "A trailing slash on the source changes this behavior to avoid creating an
-    #  additional directory level at the destination."
-    src = src.rstrip('/') + '/'
-    dst = dst.rstrip('/')
-    cmd = [
-        'rsync',
-        '--verbose',
-        '--stats',
-        '--rsh=/usr/bin/ssh',
-        '--recursive',
-        # Sync time
-        '--times',
+  """Calls rsync correctly, sync everything in src into dst.
+  http://samba.anu.edu.au/ftp/rsync/rsync.html
+
+  Args:
+  - resume will resume (append to) the file copy instead of starting over
+  - quiet remove progress, for cron job
+  - Bandwidth is in KBytes per second.
+  """
+  # "A trailing slash on the source changes this behavior to avoid creating an
+  #  additional directory level at the destination."
+  src = src.rstrip('/') + '/'
+  dst = dst.rstrip('/')
+  cmd = [
+    'rsync',
+    '--verbose',
+    '--stats',
+    '--rsh=/usr/bin/ssh',
+    '--recursive',
+    # Sync time
+    '--times',
+    # Don't keep empty dirs
+    '--prune-empty-dirs',
+    # Don't copy perms
+    '--no-perms',
+  ]
+  if delete:
+    cmd.extend(
+      [
         # Delete deleted files
         '--delete',
-        # Don't keep empty dirs
-        '--prune-empty-dirs',
-        # Don't copy perms
-        '--no-perms',
         # Create ~ files instead of deleting
         '--backup',
         # Existing excluded files will be deleted
-        '--delete-excluded']
+        '--delete-excluded',
+      ])
 
-    for exclusion in excludes:
-        cmd.extend(('--exclude', exclusion))
+  for exclusion in excludes:
+    cmd.extend(('--exclude', exclusion))
 
-    if resume:
-        # Implies --inplace and --partial
-        # Breaks "backups as hardlinks"
-        # Solution: using separate HDs requires copies anyway.
-        cmd += ['--append-verify']
-        # Search more for file move since bandwidth is scarce.
-        cmd += ['--fuzzy']
+  if resume:
+    # Implies --inplace and --partial
+    # Breaks "backups as hardlinks"
+    # Solution: using separate HDs requires copies anyway.
+    cmd += ['--append-verify']
+    # Search more for file move since bandwidth is scarce.
+    cmd += ['--fuzzy']
 
-    if not quiet:
-        cmd += ['--progress']
+  if not quiet:
+    cmd += ['--progress']
 
-    if bandwidth and ':' in src:
-        # Only limit for non local syncs.
-        cmd += ['--bwlimit=%s' % bandwidth]
+  if bandwidth and ':' in src:
+    # Only limit for non local syncs.
+    cmd += ['--bwlimit=%s' % bandwidth]
 
-    cmd += [src, dst]
-    return subprocess.call(cmd)
+  cmd += [src, dst]
+  return subprocess.call(cmd)
 
 
 def main():
-    parser = optparse.OptionParser(usage='%prog [src] [dst]')
-    parser.add_option('-q', '--quiet', action='store_true')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Specify the src and dst')
-    return rsync(args[0], args[1], quiet=options.quiet)
+  parser = optparse.OptionParser(usage='%prog [src] [dst]')
+  parser.add_option('-q', '--quiet', action='store_true')
+  parser.add_option(
+      '-d', '--delete', action='store_true',
+      help='Delete remote files that are not present locally')
+  options, args = parser.parse_args()
+  if len(args) != 2:
+    parser.error('Specify the src and dst')
+  return rsync(args[0], args[1], delete=options.delete, quiet=options.quiet)
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+  sys.exit(main())
