916b61177708e22cd7df2be84e35889d448e8db0
Marc-Antoine Ruel <maruel@gmail.com>
2014-03-10 12:45:26 -0400

    Update OSX installation instructions for user local homebrew

--- setup_scripts/install_homebrew.sh
+++ setup_scripts/install_homebrew.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-# Copyright 2012 Marc-Antoine Ruel. All Rights Reserved. Use of this
+# Copyright 2014 Marc-Antoine Ruel. All Rights Reserved. Use of this
 # source code is governed by a BSD-style license that can be found in the
 # LICENSE file.
 
 # Install homebrew locally, not system wide. Homebrew shouldn't mess with
 # /usr/local, seriously.
+#
+# .bash_aliases adds $HOME/bin/homebrew/bin to PATH.
 
 set -e
 
 cd ~/bin
 mkdir homebrew
-curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C homebrew
+curl -sL https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C homebrew
