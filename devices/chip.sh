dfa22db14ddf01087cd9c4831c21fad7072b4488
Marc-Antoine Ruel <maruel@chromium.org>
2016-12-03 21:07:37 -0500

    Fixes after actually trying the script

--- devices/chip.sh
+++ devices/chip.sh
@@ -3,7 +3,8 @@
 # source code is governed by a BSD-style license that can be found in the
 # LICENSE file.
 #
-# - Flash with https://flash.getchip.com
+# - Flash with http://flash.getchip.com
+#   - Choose the Headless image.
 # - Connect with screen /dev/ttyACM0
 # - Make sure you the C.H.I.P. has network access. This simplest is:
 #     nmcli device wifi list
@@ -13,6 +14,9 @@
 
 set -eu
 
+# Obviously don't use that on your own C.H.I.P.; that's my keys. :)
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKLhs80AouVRKus3NySEpRDwljUDC0V9dyNwhBuo4p6 maruel' >>.ssh/authorized_keys
+
 sudo apt-get update
 sudo apt-get upgrade -y
 # If you are space constrained, here's the approximative size:
@@ -20,19 +24,15 @@ sudo apt-get upgrade -y
 # python: 18MB
 # ssh:   130kB
 # tmux:  670kB
+# vim:    28MB (!)
 sudo apt-get install -y git tmux vim python ssh
 mkdir -p bin .ssh
 git clone --recurse https://github.com/maruel/bin_pub bin/bin_pub
 ./bin/bin_pub/setup_scripts/update_config.py
 
 # Do not run on C.H.I.P. Pro because of lack of space.
-sudo ./bin/bin_pub/setup/install_golang.py --system
+sudo ./bin/bin_pub/setup_scripts/install_golang.py --system
 # Temporary until Go 1.8.
 cat >> .profile <<'EOF'
 export GOPATH=$HOME/go
-EOF
-
-# Obviously don't use that on your own C.H.I.P.; that's my keys. :)
-cat > .ssh/authorized_keys <<EOF
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKLhs80AouVRKus3NySEpRDwljUDC0V9dyNwhBuo4p6 maruel
 EOF
