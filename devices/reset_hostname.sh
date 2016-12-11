d53d6eb09e59686d41d0c9462fbd5991d5fd5faf
Marc-Antoine Ruel <maruel@chromium.org>
2016-12-11 14:53:21 -0500

    Add reset_hostname.sh

---
+++ devices/reset_hostname.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2016 Marc-Antoine Ruel. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
+# Generate a hostname based on the serial number of the CPU with leading zeros
+# trimmed off, it is a constant yet unique value.
+#
+# Set a short motd.
+
+set -eu
+
+# Get the CPU serial number, otherwise the systemd machine ID.
+SERIAL="$(cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2 | sed 's/^ 0\+//')"
+if [ "$SERIAL" == "" ]; then
+  SERIAL="$(hostnamectl status | grep 'Machine ID' | cut -d ':' -f 2 | cut -c 2- | cut -c -8)"
+fi
+
+# Usually "raspberrypi" or "chip". Keep it.
+PREFIX="$(hostname)"
+
+HOST="$PREFIX-$SERIAL"
+echo "- New hostname is: $HOST"
+if [ "$(grep 'ID=' /etc/os-release)" == "ID=raspbian" ]; then
+  raspi-config nonint do_hostname $HOST
+else
+  hostnamectl set-hostname $HOST
+fi
+
+echo "- Changing MOTD"
+echo "Welcome to $HOST" > /etc/motd
