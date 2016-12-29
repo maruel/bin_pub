f64c572a337bbea57ead7db45c9e94aa0540bbdb
Marc-Antoine Ruel <maruel@gmail.com>
2016-12-28 20:29:08 -0500

    Improvements to support ODROID-C1+

--- devices/reset_hostname.sh
+++ devices/reset_hostname.sh
@@ -10,10 +10,27 @@
 
 set -eu
 
+# Automatic detection.
+DIST="$(grep '^ID=' /etc/os-release | cut -c 4-)"
+BOARD=unknown
+if [ -f /etc/dogtag ]; then
+  BOARD=beaglebone
+fi
+if [ -f /etc/chip_build_info.txt ]; then
+  BOARD=chip
+fi
+if [ $DIST==raspbian ]; then
+  BOARD=raspberrypi
+fi
+
 # Get the CPU serial number, otherwise the systemd machine ID.
 SERIAL="$(cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2 | sed 's/^[ 0]\+//')"
 if [ "$SERIAL" == "" ]; then
-  SERIAL="$(hostnamectl status | grep 'Machine ID' | cut -d ':' -f 2 | cut -c 2- | cut -c -8)"
+  SERIAL="$(hostnamectl status | grep 'Machine ID' | cut -d ':' -f 2 | cut -c 2-)"
+fi
+# On ODROID, Serial is 1b00000000000000.
+if [ "$SERIAL" == "1b00000000000000" ]; then
+  SERIAL="$(hostnamectl status | grep 'Machine ID' | cut -d ':' -f 2 | cut -c 2-)"
 fi
 
 # Cut to keep the last 4 characters. Otherwise this quickly becomes unwieldy.
@@ -21,14 +38,11 @@ fi
 # devices at once. 4 characters of hex encoded digits gives 65535 combinations.
 # Taking in account there will be at most 255 devices on the network subnet, it
 # should be "good enough". Increase to 5 if needed.
-SERIAL="$( echo $SERIAL | sed 's/.*\(....\)/\1/')"
+SERIAL="$(echo $SERIAL | sed 's/.*\(....\)/\1/')"
 
-# Usually "raspberrypi" or "chip". Keep it.
-OLD="$(hostname)"
-
-HOST="$OLD-$SERIAL"
+HOST="$BOARD-$SERIAL"
 echo "- New hostname is: $HOST"
-if [ "$(grep 'ID=' /etc/os-release)" == "ID=raspbian" ]; then
+if [ $BOARD=raspberrypi ]; then
   sudo raspi-config nonint do_hostname $HOST
 else
   # It hangs on the CHIP (?)
