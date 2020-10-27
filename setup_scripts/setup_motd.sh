5fb983e5c21d5facb5293c26c00dc31820656530
Marc-Antoine Ruel <maruel@chromium.org>
2020-10-27 08:30:27 -0400

    setup_motd.sh: silence more
    hwe-eol is just noise, I reinstall LTS often enough.
    Same for release-upgrade. I can count 2 years.

--- setup_scripts/setup_motd.sh
+++ setup_scripts/setup_motd.sh
@@ -12,6 +12,8 @@ set -eu
 sudo chmod -x /etc/update-motd.d/00-header
 sudo chmod -x /etc/update-motd.d/10-help-text
 sudo chmod -x /etc/update-motd.d/50-motd-news
+sudo chmod -x /etc/update-motd.d/91-release-upgrade
+sudo chmod -x /etc/update-motd.d/95-hwe-eol
 
 # Apologies for the swearing. This is an inside joke.
 cat << 'EOF' | sudo tee /etc/update-motd.d/20-fuck-yeah
