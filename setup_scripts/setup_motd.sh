9416380a55dd1aa929c698a6309c9ba3b4b707e6
Marc-Antoine Ruel <maruel@gmail.com>
2020-10-26 20:18:41 -0400

    Add setup_motd.sh to reduce output on login

---
+++ setup_scripts/setup_motd.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copyright 2020 Marc-Antoine Ruel. All Rights Reserved. Use of this
+# source code is governed by a BSD-style license that can be found in the
+# LICENSE file.
+
+# Remove some of the junk printed on motd.
+#
+# Specific for Ubuntu.
+
+set -eu
+
+sudo chmod -x /etc/update-motd.d/00-header
+sudo chmod -x /etc/update-motd.d/10-help-text
+sudo chmod -x /etc/update-motd.d/50-motd-news
+
+# Apologies for the swearing. This is an inside joke.
+cat << 'EOF' | sudo tee /etc/update-motd.d/20-fuck-yeah
+#!/bin/sh
+# Created by github.com/maruel/bin_pub
+echo "$(hostname) - fuck yeah!"
+EOF
+sudo chmod +x /etc/update-motd.d/20-fuck-yeah
