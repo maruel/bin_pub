5651a05844e9178a23eaf4ce7a930bfeee2b04e2
Marc-Antoine Ruel <maruel@gmail.com>
2023-10-11 14:55:51 -0400

    updates

--- rsync.py
+++ rsync.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2011 Marc-Antoine Ruel. All Rights Reserved. Use of this
 # source code is governed by a BSD-style license that can be found in the
