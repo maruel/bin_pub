504af4704bdb95f1a5f087ce1776658333671d79
Marc-Antoine Ruel <maruel@gmail.com>
2024-11-21 16:18:57 -0500

    nvim config improvements

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -1,21 +1,38 @@
--- MARUEL: Warning: Still learning.
+-- Ref: https://neovim.io/doc/user/lua-guide.html
+--
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Load all plugins.
 require("config.lazy")
 
 
+local gitsigns = require('gitsigns')
+local goformat = require('go.format')
+
 -- TODO: Take a look at https://dotfyle.com/neovim/plugins/top
+
 
 -- https://github.com/ray-x/go.nvim#run-gofmt--goimports-on-save
 local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
-  callback = function()
-    require('go.format').goimports()
-  end,
+  callback = goformat.goimports,
   group = format_sync_grp,
 })
 
+
+-- Key bindings.
+vim.keymap.set('n', '<F4>', gitsigns.blame, {})
+vim.api.nvim_set_keymap('n', '<F5>', ':GoCoverage<CR>', { noremap = true, silent = true })
+-- gd = GoToDef
+
+
 -- TODO:
 --  - :GoCoverage leaves a cover.cov file lying around.
---  - Key binding for :GoCoverage
 --  - Autoformat on save doesn't work.
---  - GoToDef working and key binding.
+--
+--
