24275c9cb8c9eae1d563ad990c4ab4f20854976a
Marc-Antoine Ruel <maruel@gmail.com>
2024-12-02 15:51:44 -0500

    nvim: Add swift, default tab to 4, add K

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -6,28 +6,44 @@
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
 
 
 -- Load all plugins.
 require("config.lazy")
 require('avante').setup()
-require('lspconfig').gopls.setup({})
+local lspconfig = require('lspconfig')
+lspconfig.gopls.setup({})
+lspconfig.sourcekit.setup({
+	capabilities = {
+		workspace = {
+			didChangeWatchedFiles = {
+				dynamicRegistration = true,
+			},
+		},
+	},
+})
 
 
 -- References to plugins to setup key bindings.
 local gitsigns = require('gitsigns')
 
 -- Key bindings.
+-- F-keys
 vim.keymap.set({'n', 'v'}, '<F3>', ':NvimTreeFindFileToggle<CR>', {noremap = true, silent = true})
 vim.keymap.set('i', '<F3>', '<Esc>:NvimTreeFindFileToggle<CR>', {noremap = true, silent = true})
 vim.keymap.set({'n', 'v', 'i'}, '<F4>', gitsigns.blame, {noremap = true, silent = true})
 vim.keymap.set({'n', 'v'}, '<F5>', ':GoCoverage<CR>', {noremap = true, silent = true})
 vim.keymap.set('i', '<F5>', '<Esc>:GoCoverage<CR>', {noremap = true, silent = true})
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
 vim.keymap.set({'n', 'v'}, '<F9>', ':bp<CR>', {noremap = true, silent = true})
 vim.keymap.set('i', '<F9>', '<Esc>:bp<CR>', {noremap = true, silent = true})
 vim.keymap.set({'n', 'v'}, '<F10>', ':bn<CR>', {noremap = true, silent = true})
 vim.keymap.set('i', '<F10>', '<Esc>:bn<CR>', {noremap = true, silent = true})
+-- Others
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {noremap = true, silent = true})
 
 
 -- Auto format on save.
@@ -40,6 +56,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 
+-- Go specific.
 local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
@@ -54,6 +71,5 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
   callback = function(ev)
     vim.opt.tabstop = 2
     vim.opt.shiftwidth = 2
-    vim.opt.smarttab = true
   end
 })
