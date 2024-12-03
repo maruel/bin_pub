92a25b5cd5494646536bf69f752da83c4ca1d205
Marc-Antoine Ruel <maruel@gmail.com>
2024-12-03 17:21:59 -0500

    Alias vi to nvim

---
+++ configs/.config/bash.d/nvim.sh
@@ -0,0 +1,10 @@
+# Copyright 2024 Marc-Antoine Ruel. All Rights Reserved. Use of this
+# source code is governed by a BSD-style license that can be found in the
+# LICENSE file.
+
+# Source: https://github.com/maruel/bin_pub
+
+if which nvim > /dev/null; then
+  alias vi=nvim
+  alias vim=nvim
+fi
