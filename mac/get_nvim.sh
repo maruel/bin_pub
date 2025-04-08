c69650108de2c382f1ff810b97ee868734b5414f
Marc-Antoine Ruel <maruel@gmail.com>
2025-04-08 19:55:05 -0400

    mac get_nvim.sh: print version

--- mac/get_nvim.sh
+++ mac/get_nvim.sh
@@ -16,6 +16,8 @@ mkdir -p nvim
 curl -sSL -o nvim/nvim.tar.gz https://github.com/neovim/neovim/releases/latest/download/nvim-macos-arm64.tar.gz
 tar -C nvim --strip-components=1 -xzf nvim/nvim.tar.gz
 rm nvim/nvim.tar.gz
+./nvim/bin/nvim --version
 
 echo "Install ripgrep, necessary for telescope.nvim"
 brew install ripgrep
+
