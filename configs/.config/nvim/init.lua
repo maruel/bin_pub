4600c45da14b85895121803751ab8b61b0ae36e3
Marc-Antoine Ruel <maruel@gmail.com>
2025-04-12 16:59:49 -0400

    neovim: maybe goimport works now?

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -177,18 +177,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		-- Auto format on save if supported.
 		-- TODO: I think I understand, there's multiple LSPs connecting!
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if not client:supports_method('textDocument/willSaveWaitUntil')
+		if not client:supports_method('textDocument/codeAction') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.code_action { context = { diagnostics = {}, only = { 'source.organizeImports' } }, apply = true, triggerKind = 2 }
+				end,
+			})
+		elseif not client:supports_method('textDocument/willSaveWaitUntil')
 			and client:supports_method('textDocument/formatting') then
 			vim.api.nvim_create_autocmd('BufWritePre', {
 				group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
 				buffer = args.buf,
 				callback = function()
-					if vim.bo.filetype == 'go' then
-						-- vim.lsp.buf.code_action { context = { diagnostics = {}, only = { 'source.organizeImports' } }, apply = true, triggerKind = 2 }
-						vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-					else
-						vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-					end
+					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
 				end,
 			})
 		end
