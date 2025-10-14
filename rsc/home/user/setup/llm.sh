11338650632621939411549eaf0cd9e3c84d572b
Marc-Antoine Ruel <maruel@gmail.com>
2025-10-14 17:53:54 -0400

    Install bubblewrap, don't install goose

--- rsc/home/user/setup/llm.sh
+++ rsc/home/user/setup/llm.sh
@@ -8,7 +8,11 @@ if ! which nvm &> /dev/null; then
 	. ~/.nvm/nvm.sh
 fi
 
-npm install -g @anthropic-ai/claude-code @google/gemini-cli @openai/codex @qwen-code/qwen-code@latest vscode-langservers-extracted
+npm install -g \
+	@anthropic-ai/claude-code \
+	@google/gemini-cli \
+	@openai/codex \
+	@qwen-code/qwen-code@latest \
+	vscode-langservers-extracted
 
-
-curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | bash
+# curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | bash
