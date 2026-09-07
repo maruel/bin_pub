07329aa27255d993221df7adcd1d9b209676a21f
Marc-Antoine Ruel <maruel@gmail.com>
2026-09-07 09:00:12 -0400

    Add linux/get_nvm.sh

--- linux/get_nodejs.sh
+++ linux/get_nodejs.sh
@@ -7,9 +7,9 @@ set -eu
 cd "$(dirname $0)"
 cd ..
 
-BASE_URL=https://nodejs.org/dist/latest/
+#BASE_URL=https://nodejs.org/dist/latest/
 BASE_URL=https://nodejs.org/dist/latest-v24.x/
-BASE_URL=https://nodejs.org/dist/latest-v26.x/
+#BASE_URL=https://nodejs.org/dist/latest-v26.x/
 NODEJS_VERSION="$(curl -sS $BASE_URL | grep -oP 'node-[^"]+-linux-x64\.tar\.xz' | head -n 1)"
 echo "Downloading $NODEJS_VERSION"
 
